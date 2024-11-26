5ffbd6527b55e8578d56fe4abc8e7f1bf8091112
MariuszMalankiewicz <m2ario123@wp.pl>
2024-11-26 12:25:57 +0100

    Integrate Vue.js with Laravel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,15 @@
 import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    vue(),
+    laravel({
+      input: [
+        'resources/js/app.js',  // Plik wejściowy Twojej aplikacji Vue
+        'resources/css/app.css', // Opcjonalnie, jeśli używasz CSS
+      ],
+    }),
+  ],
 });
