df623fcea77d9f03916439c70e09aaf4ec79f39f
MariuszMalankiewicz <m2ario123@wp.pl>
2024-12-09 13:51:47 +0100

    add vue router

---
+++ frontend/src/views/AboutView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>About</h1>
+    <p>To jest strona "O nas".</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'AboutView',
+};
+</script>
---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1 class="text-red-500">Home</h1>
+    <p>Witamy na stronie głównej!</p>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'HomeView',
+};
+</script>
