b9fcee50a628ac266a16682fc82085a1ebb09d2e
MariuszMalankiewicz <m2ario123@wp.pl>
2024-12-28 09:12:51 +0100

    update home view

--- frontend/src/views/public/Home.vue
+++ frontend/src/views/public/Home.vue
@@ -11,8 +11,11 @@
       <div>
         <div class="mt-12 max-w-[350px] lg:max-w-[400px] 2xl:max-w-[450px]">
           <p class="text-[22px] sm:text-[28px] font-bold">Cześć!</p>
-          <p class="text-[18px] sm:text-[21px]">
-            Nazywam się <span class="font-bold">Mariusz Malankiewicz</span>.
+          <p class="text-[16px] sm:text-[18px]">
+            Nazywam się
+            <span class="font-bold text-[18px] sm:text-[20px]"
+              >Mariusz Malankiewicz</span
+            >.
           </p>
           <p
             class="text-[16px] lg:text-[18px] leading-[28px] lg:leading-[32px] text-justify"
